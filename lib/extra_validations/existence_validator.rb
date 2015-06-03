5a46ef31782f81eba3a6c69d2ab72aae214a7098
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-03 06:00:13 -0300

    Add documentation for ExistenceValidator

--- lib/extra_validations/existence_validator.rb
+++ lib/extra_validations/existence_validator.rb
@@ -1,4 +1,27 @@
 module ExtraValidations
+  # Makes sure that an object/record exists.
+  #
+  # Suppose that you have the following class:
+  #
+  #   class Book
+  #     include ActiveModel::Model
+  #     include ExtraValidations
+  #
+  #     attr_accessor :author_id
+  #
+  #     validates :author_id, existence: -> (id) { Author.exists?(id) }
+  #   end
+  #
+  # This validator will execute the given block and, if it returns false, the
+  # object will not be valid:
+  #
+  #   book = Book.new
+  #   book.author_id = 100
+  #   book.valid? # => false
+  #
+  #   book.author_id = Author.first.id
+  #   book.valid? # => true
+  #
   class ExistenceValidator < ActiveModel::EachValidator
     def check_validity!
       unless options[:with].is_a?(Proc)
@@ -6,6 +29,9 @@ module ExtraValidations
       end
     end
 
+    # @param record An object that has ActiveModel::Validations included
+    # @param attribute [Symbol]
+    # @param id [Integer]
     def validate_each(record, attribute, id)
       return if id.blank?
 
