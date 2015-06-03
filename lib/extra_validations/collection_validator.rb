72837151248a2b7212d5ae164a96fd8c30175ea7
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-03 05:35:26 -0300

    Add documentation for CollectionValidator

--- lib/extra_validations/collection_validator.rb
+++ lib/extra_validations/collection_validator.rb
@@ -1,5 +1,36 @@
 module ExtraValidations
+  # Makes sure that each member of the collection is a valid object.
+  #
+  # Suppose that you have the following class:
+  #
+  #   class Book
+  #     include ActiveModel::Model
+  #     include ExtraValidations
+  #
+  #     attr_accessor :authors
+  #
+  #     validates :authors, collection: true
+  #   end
+  #
+  # This validator will call +#valid?+ for each member of the collection:
+  #
+  #   book = Book.new
+  #   book.authors = [invalid_author1, valid_author1]
+  #   book.valid? # => false
+  #
+  #   book.authors = [valid_author1, valid_author2]
+  #   book.valid? # => true
+  #
+  # Each validation error found on members will be appended on to the errors
+  # object:
+  #
+  #   book.errors.messages # => {:"authors/1/name"=>["can't be blank"]}
+  #
   class CollectionValidator < ActiveModel::EachValidator
+    # @param record An object that has ActiveModel::Validations included
+    # @param attribute [Symbol]
+    # @param collection [Array] An array of objects that have
+    #   ActiveModel::Validations included
     def validate_each(record, attribute, collection)
       return if collection.blank?
 
