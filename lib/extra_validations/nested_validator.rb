4b836c19c8da4be78517f2369f652d64166b93f8
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-03 06:03:29 -0300

    Add documentation for NestedValidator

--- lib/extra_validations/nested_validator.rb
+++ lib/extra_validations/nested_validator.rb
@@ -1,5 +1,34 @@
 module ExtraValidations
+  # Makes sure that the nested object is valid.
+  #
+  # Suppose that you have the following class:
+  #
+  #   class Book
+  #     include ActiveModel::Model
+  #     include ExtraValidations
+  #
+  #     attr_accessor :author
+  #
+  #     validates :author, nested: true
+  #   end
+  #
+  # This validator will call +#valid?+ on the nested object:
+  #
+  #   book = Book.new
+  #   book.author = invalid_author
+  #   book.valid? # => false
+  #
+  #   book.author = valid_author
+  #   book.valid? # => true
+  #
+  # Each validation error found will be appended on to the errors object:
+  #
+  #   book.errors.messages # => {:"author/name"=>["can't be blank"]}
+  #
   class NestedValidator < ActiveModel::EachValidator
+    # @param record An object that has ActiveModel::Validations included
+    # @param attribute [Symbol]
+    # @param nested An object that has ActiveModel::Validations included
     def validate_each(record, attribute, nested)
       return if !nested || nested.valid?
 
