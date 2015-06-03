8f8440378662ef269a6aa58d6ef7e313aa4f2f81
Lenon Marcel <lenon.marcel@gmail.com>
2015-06-03 05:05:26 -0300

    Add documentation for CollectionLengthValidator

--- lib/extra_validations/collection_length_validator.rb
+++ lib/extra_validations/collection_length_validator.rb
@@ -1,4 +1,27 @@
 module ExtraValidations
+  # Validates the length of a collection.
+  #
+  # Suppose that you have the following class:
+  #
+  #   class Book
+  #     include ActiveModel::Model
+  #     include ExtraValidations
+  #
+  #     attr_accessor :authors
+  #
+  #     validates :authors, collection_length: 1..5
+  #   end
+  #
+  # This validator will make sure that the _authors_ array will have at least
+  # 1 item and at most 5 items:
+  #
+  #   book = Book.new
+  #   book.authors = []
+  #   book.valid? # => false
+  #
+  #   book.authors = [Author.new]
+  #   book.valid? # => true
+  #
   class CollectionLengthValidator < ActiveModel::EachValidator
     def check_validity!
       unless options[:in].is_a?(Range)
@@ -6,6 +29,9 @@ module ExtraValidations
       end
     end
 
+    # @param record An object that has ActiveModel::Validations included
+    # @param attribute [Symbol]
+    # @param collection [Array]
     def validate_each(record, attribute, collection)
       min = options[:in].begin
       max = options[:in].end
